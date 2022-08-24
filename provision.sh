55212f39d8f3244b854e7631e27aed855c6e7ce4
Ayanle Salad <104793540+ASalad42@users.noreply.github.com>
2022-08-24 14:52:42 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -20,24 +20,25 @@
  sudo apt-get upgrade -y
 
  # automate reverse proxy and mongod
- sudo cp -f app/app/rp_file /etc/nginx/sites-available/default
+ sudo cp -f app/rp_file /etc/nginx/sites-available/default
+# sudo cp -f app/app/rp_file /etc/nginx/sites-available/default
  sudo systemctl restart nginx 
  sudo systemctl enable nginx 
 
 
- sudo echo "export DB_HOST=mongodb://192.168.10.150:27017/posts" >> /etc/.bashrc # print(echo) the dbhost connecttion into the .bashrc file 
- source /etc/.bashrc # use this source command if you want to apply the changes during the current session
+# sudo echo "export DB_HOST=mongodb://192.168.10.150:27017/posts" >> /etc/.bashrc # print(echo) the dbhost connecttion into the .bashrc file 
+# source /etc/.bashrc # use this source command if you want to apply the changes during the current session
  
 #To set permanent environment variables for all users, create an .sh file in the /etc/profile.d folder: `sudo nano /etc/profile.d/[filename].sh`?????
 
  
  cd app
- cd app
+ # cd app
  sudo apt-get install npm -y # solution on stackoverflow 
  npm install express -y
  npm install mongoose -y
- cd seeds
- node seed.js
- cd ..
- npm install
- npm start -d # detached, still have control of the terminal 
+ #cd seeds
+ #node seed.js
+ #cd ..
+# npm install
+ #npm start -d # detached, still have control of the terminal 
